06239e1efcbd3e025601ec39311d977fe8c1cd58
Sudipta Dhara <z.sudipta.dhara@gree.co.jp>
2018-03-31 10:06:03 +0530

    First Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,9 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private iab: InAppBrowser) {
+  	const browser = this.iab.create('https://www.techiediaries.com','_self',{location:'no'}); 
+	browser.close();
   }
 
 }
